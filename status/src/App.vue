8baea7853c80a0b2b06e6f84149d8430940ceb58
GodsonAddy <70777404+GodsonAddy@users.noreply.github.com>
2021-07-30 13:37:23 +0000

    first commit

---
+++ status/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <inProgress />
+</template>
+
+<script>
+import  inProgress from './components/inProgress.vue'
+
+export default {
+  name: 'App',
+  components: {
+    inProgress
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
